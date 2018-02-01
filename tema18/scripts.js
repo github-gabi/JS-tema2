9a099ee6df181b48f2a072719b04f6d9b4bb0e00
gabi-github <g_bartalis@yahoo.com>
2018-02-01 06:49:50 +0200

    tema 18

--- tema18/scripts.js
+++ tema18/scripts.js
@@ -1,7 +1,12 @@
-for (let j = 0; j < 10; j++) {
-    $('main').append('<div class="keysLetters>' + j + '</div>')
+/* for (let i = String.fromCharCode(65); i < String.fromCharCode(93); i++) {
+    $('#keysLetters').append('<div class="keys">' + String.fromCharCode(i) + '</div>')
+} */
+
+for (let i = 65; i < 94; i++) {
+    var letters = String.fromCharCode(i);
+    $('#keysLetters').append('<div class="keys">' + letters + '</div>')
 }
 
-for (let i = String.fromCharCode(65); i < String.fromCharCode(93); i++) {
-    $('.keysLetters').append('<div class="keysNumbers>' + i + '</div>')
+for (let j = 0; j < 10; j++) {
+    $('#keysNumbers').append('<div class="keys">' + j + '</div>')
 }
