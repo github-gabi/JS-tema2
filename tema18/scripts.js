e4b31806cdd06cddbeb7c93cd2c04dc12c53bb02
gabi-github <g_bartalis@yahoo.com>
2018-02-01 23:13:37 +0200

    tema18

--- tema18/scripts.js
+++ tema18/scripts.js
@@ -1,12 +1,24 @@
-/* for (let i = String.fromCharCode(65); i < String.fromCharCode(93); i++) {
-    $('#keysLetters').append('<div class="keys">' + String.fromCharCode(i) + '</div>')
-} */
 
-for (let i = 65; i < 94; i++) {
+for (let i = 65; i < 97; i++) {
     var letters = String.fromCharCode(i);
-    $('#keysLetters').append('<div class="keys">' + letters + '</div>')
+    $('#keysLetters').append('<div class="keys">' + letters + '</div>');
 }
 
 for (let j = 0; j < 10; j++) {
-    $('#keysNumbers').append('<div class="keys">' + j + '</div>')
+    $('#keysNumbers').append('<div class="keys">' + j + '</div>');
+}
+
+$('#keysLetters').append('<div class="keys space">' + 'space' + '</div>');
+$('#keysLetters').append('<div class="keys backspace">' + '&#8678;' + '</div>');
+
+$(function() {
+    createButtonEvents();
+})
+
+function createButtonEvents() {
+    $('.keys').map((index, item) => {
+        $(item).click(function() {
+            grab($(this));
+        });
+    )};
 }
