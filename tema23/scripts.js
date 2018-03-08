7f62fbe1faf3c1967cfb1dd3bf50a1ffa2a373fd
gabi-github <g_bartalis@yahoo.com>
2018-03-08 07:05:26 +0200

    tema23

--- tema23/scripts.js
+++ tema23/scripts.js
@@ -16,6 +16,21 @@ const displayRandomNumber = () => {
         });
     };
 
+    let randomTop = getRandomNumber(0, displayHeight);
+    let randomLeft = getRandomNumber(0, displayWidth);
+
+    let colorRed = getRandomNumber(0, 255);
+    let colorGreen = getRandomNumber(0, 255);
+    let colorBlue = getRandomNumber(0, 255);
+
+    randomNumber.css({
+        top: randomTop + 'px',
+        left: randomLeft + 'px',
+
+        color: rgb(colorRed, colorGreen, colorBlue),
+
+        fontSize: randomNumber + 'px'
+    });
 
 };
 
